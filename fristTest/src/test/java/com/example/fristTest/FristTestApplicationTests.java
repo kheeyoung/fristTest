8bc4b4b389d3aef457e19e670f8560a55fccaecf
kheeyoung <talent8661@gmail.com>
2024-01-13 19:48:50 +0900

    Initial commit

---
+++ fristTest/src/test/java/com/example/fristTest/FristTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fristTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FristTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
