7a142cf22ec46ce4fd71c0c713015fd2e0e1fe1b
kheeyoung <talent8661@gmail.com>
2024-01-18 15:26:16 +0900

    Spring Boot

---
+++ fristTest/src/main/java/com/example/fristTest/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.example.fristTest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+@GetMapping("/main")
+    public String main(Model model){
+        model.addAttribute("UserName","ghgh가가");
+
+        return "main"; //templates/main.mustache 를 브라우저로 전송시켜줌
+    }
+}
